a90cb8ab63e4eac21ed0ace76a6fb1b6c0309f4e
Braden Douglass <braden.douglass@gmail.com>
2021-02-15 14:48:57 -0500

    First System Test.
    Create a system test to wrap the concept of master watch listing and
    eventually clicking on one to expand the view into a detail list of the
    watch.

---
+++ test/system/viewing_watch_details_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class ViewingWatchDetailsTest < ApplicationSystemTestCase
+  test "viewing list of watches" do
+    visit watch_path
+    assert_text ::Watch::Case.first.name
+  end
+end
