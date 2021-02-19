435f54db7fb073f2ede63b697246e8201deacdc0
Braden Douglass <braden.douglass@gmail.com>
2021-02-18 23:36:38 -0500

    Add Some Simple DB Seeds.
    Nothing too complicated here. Just something to see what Tailwind is
    doing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+::Watch::Case.create({
+  manufacturer: "Grand Seiko",
+  reference: "5722-9991",
+  purchase_date: 3.years.ago,
+  price: 300000
+})
+
+::Watch::Case.create({
+  manufacturer: "Grand Seiko",
+  reference: "SBGA429",
+  purchase_date: 3.days.ago,
+  price: 300000
+})
