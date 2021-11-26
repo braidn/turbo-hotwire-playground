af95e8c0021f916d8035020660ded2f929eaf2d4
Braden Douglass <braden.douglass@gmail.com>
2021-11-26 14:52:40 -0500

    Lint Commit.
    Small change to the tests when Viewing a watch.

--- test/system/viewing_watch_details_test.rb
+++ test/system/viewing_watch_details_test.rb
@@ -1,12 +1,12 @@
 require "application_system_test_case"
 
 class ViewingWatchDetailsTest < ApplicationSystemTestCase
-  test "viewing list of watches" do
+  test 'viewing list of watches' do
     visit root_path
     assert_text ::Watch::Case.first.reference
   end
 
-  test "viewing the details of a single watch" do
+  test 'viewing the details of a single watch' do
     watch = ::Watch::Case.first
 
     visit root_path
